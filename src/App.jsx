1a2d00fb80fc09937b675e8a940d5e237854163c
VISHAL MAURYA <vishalmaurya7973@gmail.com>
2026-05-24 02:08:05 +0530

    drag and drop completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,55 @@ import './App.css'
 import tasks from "./data/tasks"
 import columns from './data/columns';
 import TaskColumn from './components/TaskColumn';
+import { DragDropProvider } from '@dnd-kit/react';
 
 function App() {
   const [data, setData] = useState(tasks);
-
   return (
-    <div className="grid grid-cols-3 gap-4 h-screen p-4 bg-gray-100">
+    <DragDropProvider
+      onDragEnd={(event) => {
+        if (event.canceled) return;
 
-      {
-        columns.map((column) => {
-          return <TaskColumn
-            key={column.id}
-            title={column.title}
-            headerColor={column.headerColor}
-            bodyColor={column.bodyColor}
-            tasks={
-              data.filter(
-                (task) => task.status === column.status
-              )
+        const { source, target } = event.operation;
+        if (!target) return;
+
+        setData((prev) =>
+          prev.map((task) => {
+            console.log(task.id, source.id, target.id);
+
+            if (task.id === source.id) {
+              return {
+                ...task,
+                status: target.id,
+              };
             }
-          />
-        })
-      }
 
-    </div>
+            return task;
+          })
+        );
+      }}
+    >
+      <div className="grid grid-cols-3 gap-4 h-screen p-4 bg-gray-100">
+
+        {
+          columns.map((column) => {
+            return <TaskColumn
+              key={column.id}
+              id={column.id}
+              title={column.title}
+              headerColor={column.headerColor}
+              bodyColor={column.bodyColor}
+              tasks={
+                data.filter(
+                  (task) => task.status === column.status
+                )
+              }
+            />
+          })
+        }
+
+      </div>
+    </DragDropProvider>
   )
 }
 
--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import { Card } from './Card'
+import { useDroppable } from '@dnd-kit/react';
 
-const TaskColumn = ({ title, headerColor, bodyColor, tasks }) => {
+const TaskColumn = ({ id, title, headerColor, bodyColor, tasks }) => {
+
+    const { ref } = useDroppable({
+        id,
+    });
+
     return (
         <div className="grid grid-rows-[60px_1fr] gap-4">
             <div className={`${headerColor} rounded-xl flex items-center justify-center text-xl font-bold`}>
                 {title}
             </div>
 
-            <div className={`${bodyColor} rounded-xl p-4 overflow-y-auto`}>
+            <div ref={ref} className={`${bodyColor} rounded-xl p-4 overflow-y-auto`}>
                 {
                     tasks.map((task) => {
                         return <Card
